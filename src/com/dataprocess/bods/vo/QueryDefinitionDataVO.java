3b8505dcaf92545ae3fa4020ab6335ed9c1eb947
Manoj Prabhakar M <manoj.mano27@gmail.com>
2016-08-20 17:24:50 +0530

    Worked for show Data List correct order

--- src/com/dataprocess/bods/vo/QueryDefinitionDataVO.java
+++ src/com/dataprocess/bods/vo/QueryDefinitionDataVO.java
@@ -1,15 +1,28 @@
 package com.dataprocess.bods.vo;
 
+import java.util.List;
+
 public class QueryDefinitionDataVO {
 	
 	private String dataValue;
 
+	private List<QueryDefinitionDataVO> queryDefinitionDataVOList;
+	
 	public String getDataValue() {
 		return dataValue;
 	}
 
 	public void setDataValue(String dataValue) {
 		this.dataValue = dataValue;
+	}
+
+	public List<QueryDefinitionDataVO> getQueryDefinitionDataVOList() {
+		return queryDefinitionDataVOList;
+	}
+
+	public void setQueryDefinitionDataVOList(
+			List<QueryDefinitionDataVO> queryDefinitionDataVOList) {
+		this.queryDefinitionDataVOList = queryDefinitionDataVOList;
 	}
 
 }
